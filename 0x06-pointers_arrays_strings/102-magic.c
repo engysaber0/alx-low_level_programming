7dbdff99a881a9621a09b5420cc2b61c43b15712
engysaber0 <engy8174@gmail.com>
2023-08-24 04:58:40 -0700

    pointers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,6 +11,16 @@ int a[5];
 
 a[2] = 1024;
 p = &n;
+/**
+ * write code here
+ * Remember:
+ * You are not allowed to use the variable a
+ * You are not allowed to modify the variable p
+ * You can only write one statement
+ * You are not allowed to use ,
+ * You are not allowed to code anything else 
+ * than the line of expected line of code at the expected line
+*/
 *(p + 5) = 98;
 printf("a[2] = %d\n", a[2]);
 return (0);

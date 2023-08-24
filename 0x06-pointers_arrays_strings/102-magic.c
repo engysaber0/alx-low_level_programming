4feae998047f44f71823c936943a16d79604cacc
engysaber0 <engy8174@gmail.com>
2023-08-24 14:05:40 -0700

    pointets

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 /**
- * main - prints a[2] = 98
+ * main - change the value
  * Return: Always 0 (Success)
 */
 int main(void)
 {
 int n;
-int *p;
 int a[5];
+int *p;
 
 a[2] = 1024;
 p = &n;
 /*
- * write code here...
+ * write your line of code here...
  * Remember:
- * - You are not allowed to use the variable a
- * - You are not allowed to modify the variable p
- * - You can only write one statement
- * - You are not allowed to use ,
- * - You are not allowed to code anything else 
- * than the line of expected line of code at the expected line
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
  */
 *(p + 5) = 98;
-/*...80 that prints 98\n */
+ /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }

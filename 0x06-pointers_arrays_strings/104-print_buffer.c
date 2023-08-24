5c59fda2b99382581ecf9160c4111861033ec954
engysaber0 <engy8174@gmail.com>
2023-08-24 14:51:56 -0700

    pointets

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,32 +8,30 @@
 */
 void print_buffer(char *b, int size)
 {
-int i, j, n;
-n = 0;
+int i, j, o;
+o = 0;
 
 if (size <= 0)
 {
 printf("\n");
 return;
 }
-while (n < size)
+while (o < size)
 {
-j = size - n < 10 ? size - n : 10;
-printf("%08x: ", n);
+j = size - o < 10 ? size - o : 10;
+printf("%08x: ", o);
 for (i = 0; i < 10; i++)
 {
 if (i < j)
-printf("%02x", *(b + n + i));
+printf("%02x", *(b + o + i));
 else
 printf(" ");
 if (i % 2)
-{
 printf(" ");
-}
 }
 for (i = 0; i < j; i++)
 {
-int c = *(b + n + i);
+int c = *(b + o + i);
 
 if (c < 32 || c > 132)
 {
@@ -42,6 +40,6 @@ c = '.';
 printf("%c", c);
 }
 printf("\n");
-n += 10;
+o += 10;
 }
 }

3d27381e2f955b0afc20dea27b9fe5e1958173b3
engysaber0 <engy8174@gmail.com>
2023-08-24 05:12:30 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_buffer - prints a buffer.
+ * @size: size
+ * @b: buffer
+ * Return :Nothing
+*/
+void print_buffer(char *b, int size)
+{
+int i, j, b;
+n = 0;
+
+if (size <= 0)
+{
+printf("\n");
+return;
+}
+while (n < size)
+{
+j = size - n < 10 ? size - n : 10;
+printf("%08x: ", n);
+for (i = 0; i < 10; i++)
+{
+if (i < j)
+printf("%2x", *(b + n + i));
+else
+printf(" ");
+if (i % 2)
+{
+printf(" ");
+}
+}
+for (i = 0; i < j; i++)
+{
+int c = *(b + n + i);
+
+if (c < 32 || c > 132)
+{
+c = '.';
+}
+printf("%c", c);
+}
+printf("\n");
+n = n + 10;
+}
+}

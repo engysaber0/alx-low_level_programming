2dcc9e4be2e349ff899eb7dec9ee3825244fccef
engysaber0 <engy8174@gmail.com>
2023-08-24 04:29:41 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints a[2] = 98
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n, *p;
+int a[5];
+
+a[2] = 1024;
+p = &n;
+*(p + 5) = 98;
+printf("a[2] = %d\n", a[2]);
+return (0);
+}

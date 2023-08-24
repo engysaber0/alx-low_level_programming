26e88910285bc632f38c7f48cd560c02d1534fdc
engysaber0 <engy8174@gmail.com>
2023-08-24 05:56:57 -0700

    pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,41 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * */
+ * isLower - determine if lowercase
+ * @c:char
+ * Return: 1 if true, 0 is false.
+*/
+int isLower(char c)
+{
+return (c >= 97 && c <= 122);
+}
+int isDelimiter(char c)
+{
+int i;
+char delimiter[] = " \t\n,.!?\"{}()";
+
+for (i = 0; i < 12; i++)
+if (c == delimiter[i])
+return (1);
+return (0);
+}
+char *cap_string(char *s)
+{
+int check = 1;
+char *ptr = s;
+
+while (*s)
+{
+if (isDelimiter(*s))
+check = 1;
+else if (isLower(*s) && check)
+{
+*s -= 32;
+check = 0;
+}
+else
+check = 0;
+s++;
+}
+return (ptr);
+}

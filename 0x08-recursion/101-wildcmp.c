f8934e480b8e6d8f46206f487789d85f1909269e
engysaber0 <engy8174@gmail.com>
2023-08-30 08:04:59 -0700

    recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,59 @@
+#include "main.h"
+/**
+ * move_past_star - interates past asterisk
+ * @s2: second string
+ * Return: pointer past star
+*/
+char *move_past_star(char *s2)
+{
+if (*s2 == '*')
+return (move_past_star(s2 + 1));
+else
+return (s2);
+}
+/**
+ * inception - makes magic with a reality
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 or 0
+*/
+int inception(char *s1, char *s2)
+{
+int ret = 0;
+
+if (*s1 == 0)
+return (0);
+if (*s1 == *s2)
+ret = ret + wildcmp(s1 + 1, s2 + 1);
+ret = ret + inception(s1 + 1, s2);
+return (ret);
+}
+/**
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 or 0
+*/
+int wildcmp(char *s1, char *s2)
+{
+int ret = 0;
+
+if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+return (1);
+if (*s1 == *s2)
+{
+if (!*s1)
+return (1);
+return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+}
+if (!*s1 || !s2)
+return (0);
+if (*s2 == '*')
+{
+s2 = move_past_star(s2);
+if (*s1 == *s2)
+ret = ret + inception(s1, s2);
+return (!!ret);
+}
+return (0);
+}

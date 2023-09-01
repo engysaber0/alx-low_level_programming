177be21b53108f655ebc4b549c697228b1b0304f
engysaber0 <engy8174@gmail.com>
2023-09-01 09:31:05 -0700

    0x0A

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype>
+/**
+ * check_num - check - string
+ * @str: array
+ * Return: 0
+*/
+int check_num(char *str)
+{
+unsigned int c;
+
+c = 0;
+while (c < strlen(str))
+{
+if (!isdigit(str[c]))
+{
+return (0);
+}
+c++;
+}
+return (1);
+}
+/**
+ * main - print name
+ * @argc: count
+ * @argv: arguments
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int c;
+int str_to_int;
+int sum = 0;
+
+c = 1;
+while (c < argc)
+{
+if (check_num(argv[c]))
+{
+str_to_int = atoi(argv[c]);
+sum += str_to_int;
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+c++;
+}
+printf("%d\n", sum);
+return (0);
+}

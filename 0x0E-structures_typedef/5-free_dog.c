ac9d5b117924f36165934cd73496ec649f889feb
engysaber0 <engy8174@gmail.com>
2023-09-11 11:17:09 -0700

    Write a function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees dogs
+ * @d: yo dawg
+ * Return: nothing
+*/
+void free_dog(dog_t *d)
+{
+if (d)
+{
+if (d->name)
+free(d->name);
+if (d->owner)
+free(d->owner);
+free(d);
+}

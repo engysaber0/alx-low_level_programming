c35120fa54f302fa30b54a77ba734e4a186cf345
engysaber0 <engy8174@gmail.com>
2023-09-13 12:02:46 -0700

    Write a program that performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - add 2 int
+ * @a: first int
+ * @b: second int
+ * Return: result
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - sub of 2 int
+ * @a: first int
+ * @b: second int
+ * Return: difference
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - mul 2 int
+ * @a: first int
+ * @b: second int
+ * Return: mul
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - div 2 int
+ * @a: first int
+ * @b: second int
+ * Return: div
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - mod 2 int
+ * @a: first int
+ * @b: second int
+ * Return: mod
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}

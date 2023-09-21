2bc736c8904a65b1f5bbc1a6c9253832c51207ca
engysaber0 <engy8174@gmail.com>
2023-09-21 14:32:03 -0700

    tasks

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * list_len - a function to return length
+ * @h: the head pointer
+ * Return: length
+*/
+size_t list_len(const list_t *h)
+{
+size_t length;
+
+for (length = 0; h; length++)
+h = h->next;
+return (i);
+}

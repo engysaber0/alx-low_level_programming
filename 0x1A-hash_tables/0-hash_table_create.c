5e8831b0cc62f33873d14f85363b6a1671757408
engysaber0 <engy8174@gmail.com>
2024-06-13 14:55:12 -0700

    hash_table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,32 +1,25 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table.
- * @size: size of the hash table to create.
- * Return: a pointer to the hash table.
+ * hash_table_create - Creates a new hash table.
+ * @size: The size of the hash table array.
+ *
+ * Return: pointer to the newly created hash table.
+ * or NULL if an error occurs.
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-unsigned long int i;
-hash_table_t *new_hashtable;
+	hash_table_t *ht;
+	unsigned long int i;
 
-if (size <= 0)
-return (NULL);
-
-new_hashtable = malloc(sizeof(hash_table_t));
-if (!new_hashtable)
-return (NULL);
-
-new_hashtable->size = size;
-new_hashtable->array = malloc(size * sizeof(hash_node_t *));
-if (!new_hashtable->array)
-{
-free(new_hashtable);
-return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+	return (ht);
 }
-for (i = 0; i < size; i++)
-new_hashtable->array[i] = NULL;
-return (new_hashtable);
-}
-
